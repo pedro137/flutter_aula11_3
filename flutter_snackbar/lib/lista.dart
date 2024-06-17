48f479b71fd298782c860c12dc9a91ad100f5637
maurojh <maurojh@gmail.com>
2024-06-17 11:26:59 -0300

    Início

---
+++ flutter_snackbar/lib/lista.dart
@@ -0,0 +1,21 @@
+class Lista {
+  List<String> _itens = [];
+
+  Lista() {
+    novo();
+  }
+
+  List<String> getItens() {
+    return _itens;
+  }
+
+  String getElemento(int pos) {
+    return _itens[pos];
+  }
+
+  void novo() {
+    DateTime agora = DateTime.now();
+    String tempo = '${agora.hour}:${agora.minute}:${agora.second}';
+    _itens.add(tempo);
+  }
+}
